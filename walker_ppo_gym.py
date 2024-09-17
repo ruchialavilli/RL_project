a66e324ddb78beec9622a6b46fc868cbd7535e7d
ruchialavilli <ruchi.alavilli@gmail.com>
2024-09-17 16:02:09 -0700

    adding recurrent PPO and changing reward metrics

--- walker_ppo_gym.py
+++ walker_ppo_gym.py
@@ -4,12 +4,11 @@ import sys
 sys.path.insert(0, '/opt/anaconda3/envs/latest')
 import gymnasium as gym
 import numpy as np
-import wandb
+import wand
 import logging
-from sb3_contrib import RecurrentPPO
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.evaluation import evaluate_policy
-# from wandb.integration.sb3 import WandbCallback
+# from wand.integration.sb3 import WandbCallback
 from stable_baselines3.common.logger import configure
 # import pdb
 from stable_baselines3 import PPO
@@ -28,7 +27,7 @@ def make_env():
 
 run = wandb.init(
     project="bipedal_walker_ppo_gym",
-    config = {"policy": "PPO_MlpLstmPolicy", "learning_rate": 0.000827, "gamma":0.96979, "gae_lambda":0.92680, "ent_coef":.0000199},
+    config = {"policy": "PPO_MlpPolicy", "learning_rate": 0.000827, "gamma":0.96979, "gae_lambda":0.92680, "ent_coef":.0000199},
     monitor_gym=True,
     sync_tensorboard=True,
     save_code=True
@@ -41,7 +40,7 @@ env = VecVideoRecorder(env, f"videos/{run.id}", record_video_trigger = lambda x:
 # Instantiate the agent without noise
 #tmp_path = "/tmp/sb3_log/"
 #new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
-model = RecurrentPPO("MlpLstmPolicy", env, learning_rate= 0.000827, gamma = 0.98, gae_lambda= 0.92680, ent_coef= 0.0000199, verbose=1, tensorboard_log=f"runs/ppo_gym")
+model = PPO("MlpPolicy", env, learning_rate= 0.000827, gamma = 0.96979, gae_lambda= 0.92680, ent_coef= 0.0000199, verbose=1, tensorboard_log=f"runs/ppo_gym")
 #model.set_logger(new_logger)
 
 # Train the agent
@@ -57,7 +56,7 @@ vec_env = model.get_env()
 del model # remove to demonstrate saving and loading
 
 # Load the agent
-model = RecurrentPPO.load("walk_ppo_gym")
+model = PPO.load("walk_ppo_gym")
 
 # Evaluate the agent
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
@@ -66,17 +65,10 @@ wandb.log({"mean_reward": mean_reward, "std_reward": std_reward})
 print(f"Mean reward: {mean_reward} +/- {std_reward}")
 obs = vec_env.reset()
 
-# cell and hidden state of the LSTM
-lstm_states = None
-num_envs = 1
-
-# Episode start signals are used to reset the lstm states
-episode_starts = np.ones((num_envs,), dtype=bool)
 reward_total = 0
 for _ in range(1000): 
-    action, lstm_states = model.predict(obs, state=lstm_states, episode_start=episode_starts, deterministic=True)
+    action, _states = model.predict(obs)
     obs, rewards, dones, info = vec_env.step(action)
-    episode_starts = dones
     reward_total += rewards
     wandb.log({"returns" : reward_total, "reward": rewards})
     # print(rewards)
--- walker_recurrent_ppo_gym.py
+++ walker_recurrent_ppo_gym.py
@@ -1,18 +1,19 @@
-# sac
+# ppo - no noise
 # /opt/anaconda3/bin/python
 import sys
 sys.path.insert(0, '/opt/anaconda3/envs/latest')
 import gymnasium as gym
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.callbacks import EvalCallback
 import numpy as np
 import wandb
+import logging
+from sb3_contrib import RecurrentPPO
+from stable_baselines3.common.callbacks import EvalCallback
+from stable_baselines3.common.evaluation import evaluate_policy
 # from wandb.integration.sb3 import WandbCallback
+from stable_baselines3.common.logger import configure
 # import pdb
-from stable_baselines3 import SAC
+from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecVideoRecorder
-from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
-
 
 # Setting the seed for reproducibility
 seed = 42
@@ -26,40 +27,37 @@ def make_env():
     return env
 
 run = wandb.init(
-    project="bipedal_walker_sac",
-    config = {
-        "policy": "SAC_MlpPolicy",
-        "noise_sigma": 0.1,
-    },
+    project="bipedal_walker_ppo_gym",
+    config = {"policy": "PPO_MlpLstmPolicy", "learning_rate": 0.000827, "gamma":0.96979, "gae_lambda":0.92680, "ent_coef":.0000199},
     monitor_gym=True,
     sync_tensorboard=True,
     save_code=True
+
 )
 
 env = DummyVecEnv([make_env])
 env = VecVideoRecorder(env, f"videos/{run.id}", record_video_trigger = lambda x: x % 2000 == 0, video_length=200)
 
-# The noise objects for SAC
-n_actions = env.action_space.shape[-1]
-action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
-
-# Instantiate the agent with noise
-model = SAC("MlpPolicy", env, action_noise=action_noise, verbose=1, tensorboard_log=f"runs/sac")
+# Instantiate the agent without noise
+#tmp_path = "/tmp/sb3_log/"
+#new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+model = RecurrentPPO("MlpLstmPolicy", env, learning_rate= 0.000827, gamma = 0.98, gae_lambda= 0.92680, ent_coef= 0.0000199, verbose=1, tensorboard_log=f"runs/ppo_gym")
+#model.set_logger(new_logger)
 
 # Train the agent
-model.learn(total_timesteps=300000, 
-            #callback=WandbCallback(gradient_save_freq=200, model_save_path=f"models/{run.id}", verbose=0),
+model.learn(total_timesteps= 300000,
+            #callback=WandbCallback(gradient_save_freq=200, model_save_path=f"models/{run.id}", verbose=2
             callback=EvalCallback(env, best_model_save_path=f"models/best/{run.id}", log_path="./evalLogs/", eval_freq=1000)
 )
 
 # Save the agent
-model.save("walk_sac")
+model.save("walk_ppo_gym")
 vec_env = model.get_env()
 
 del model # remove to demonstrate saving and loading
 
 # Load the agent
-model = SAC.load("walk_sac")
+model = RecurrentPPO.load("walk_ppo_gym")
 
 # Evaluate the agent
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
@@ -67,11 +65,20 @@ wandb.log({"mean_reward": mean_reward, "std_reward": std_reward})
 
 print(f"Mean reward: {mean_reward} +/- {std_reward}")
 obs = vec_env.reset()
+
+# cell and hidden state of the LSTM
+lstm_states = None
+num_envs = 1
+
+# Episode start signals are used to reset the lstm states
+episode_starts = np.ones((num_envs,), dtype=bool)
 reward_total = 0
-for _ in range(1000):
-    action, _states = model.predict(obs)
+for _ in range(1000): 
+    action, lstm_states = model.predict(obs, state=lstm_states, episode_start=episode_starts, deterministic=True)
     obs, rewards, dones, info = vec_env.step(action)
+    episode_starts = dones
     reward_total += rewards
+    wandb.log({"returns" : reward_total, "reward": rewards})
     # print(rewards)
     if dones:
         reward_total = 0
